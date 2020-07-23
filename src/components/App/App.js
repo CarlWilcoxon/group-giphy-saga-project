297da2e56aa83b143156bb11c7f80b0e2deb8afa
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-07-23 10:52:48 -0500

    Search bar functions, but doesn't display results.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Search from '../Search/Search';
 
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div>
         <h1>Giphy Search!</h1>
+        <Search />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -15,8 +15,20 @@ import { takeEvery, put } from 'redux-saga/effects';
 // Saga Setup #4 create the "main" saga function
 //   common names for this are `watcherSaga` and `rootSaga`
 function* watcherSaga() {
+  yield takeEvery( 'SET_SEARCH', searchGifSaga);
   // yield takeEvery( 'FETCH_GIF', getGifsSaga );
 }
+
+function* searchGifSaga(action){
+    //us try/catch for errors - replaces promise .then & .catch
+    try {
+      const response = yield axios.get('/api/search/' + action.payload);
+      // in Sagas, replace `dispatch` with `put`
+      yield put({ type: 'SET_GIFS', payload: response.data });
+    } catch (error) {
+        console.log('error with plant get request', error);
+    }
+  }
 
 // //Saga to get plants from the server AJAX (axios)
 // function* getGifsSaga(){
@@ -54,6 +66,7 @@ const sagaMiddleware = createSagaMiddleware();
 
 const storeInstance = createStore(
   combineReducers({
+    gifList
   }),
   applyMiddleware(sagaMiddleware, logger)
 );
